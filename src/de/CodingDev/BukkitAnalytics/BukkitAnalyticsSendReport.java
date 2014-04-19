274f77f18a602a9d4c57c9b35ad5f2561da50957
R3N3PDE <rene.preuss@hostinfin.net>
2014-04-19 20:52:08 +0200

    Add Post sender

--- src/de/CodingDev/BukkitAnalytics/BukkitAnalyticsSendReport.java
+++ src/de/CodingDev/BukkitAnalytics/BukkitAnalyticsSendReport.java
@@ -1,14 +1,22 @@
 package de.CodingDev.BukkitAnalytics;
 
-public class BukkitAnalyticsSendReport extends Thread{
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
+public class BukkitAnalyticsSendReport extends Thread{
 	private boolean b;
 	private BukkitAnalyticsReport analyticsReport;
 	private BukkitAnalytics bukkitAnalytics;
+	private String USER_AGENT = "BukkitAnalytics/Unknow";
 
 	public BukkitAnalyticsSendReport(BukkitAnalytics bukkitAnalytics, BukkitAnalyticsReport analyticsReport) {
 		this.bukkitAnalytics = bukkitAnalytics;
 		this.analyticsReport = analyticsReport;
+		USER_AGENT = "BukkitAnalytics/" + bukkitAnalytics.getDescription().getVersion();
 	}
 
 	public void setRunning(boolean b) {
@@ -19,7 +27,7 @@ public class BukkitAnalyticsSendReport extends Thread{
 		if(bukkitAnalytics.isDebugMode()){
 			return "http://127.0.0.1/api/v1/report.php";
 		}else{
-			return "http://127.0.0.1/api/v1/report.php";
+			return "http://BukkitAnalytics.net/api/v1/report.php";
 		}
 	}
 	
@@ -44,9 +52,46 @@ public class BukkitAnalyticsSendReport extends Thread{
 	public void sendReport() {
 		bukkitAnalytics.sendDebugMessage("Sending BukkitAnalytics Report...");
 		String jsonReport = analyticsReport.getJsonReportData();
-		analyticsReport.clearReport();
-		
 		bukkitAnalytics.sendDebugMessage("Report Data: " + jsonReport);
+		
+		try{
+			URL obj = new URL(getTrackingAPIServer());
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			//add reuqest header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+	 
+			String urlParameters = "jsonReport=" + URLEncoder.encode(jsonReport);
+	 
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+	 
+			int responseCode = con.getResponseCode();
+			bukkitAnalytics.sendDebugMessage("Sending 'POST' request to URL : " + getTrackingAPIServer());
+			bukkitAnalytics.sendDebugMessage("Post parameters : " + urlParameters);
+			bukkitAnalytics.sendDebugMessage("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+	 
+			//print result		
+			bukkitAnalytics.sendDebugMessage("Response Data: " + response.toString());
+			analyticsReport.clearReport();
+		}catch(Exception sendError){
+			
+		}
 		bukkitAnalytics.sendDebugMessage("Done!");
 	}
 	
